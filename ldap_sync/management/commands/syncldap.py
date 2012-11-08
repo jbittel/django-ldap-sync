966d3b26e265206c05c7499c8a0ba0a9b7a4ecbb
Jason Bittel <jason.bittel@gmail.com>
2012-11-08 08:26:33 -0800

    Reorganize management command class methods

--- ldap_sync/management/commands/syncldap.py
+++ ldap_sync/management/commands/syncldap.py
@@ -8,17 +8,70 @@ from django.contrib.auth.models import User
 from django.contrib.auth.models import SiteProfileNotAvailable
 
 
-logger = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
 
 class Command(NoArgsCommand):
     help = "Synchronize users and groups with an authoritative LDAP server"
 
     def handle_noargs(self, **options):
-        ldap_users = self.get_ldap_users()
-        logger.info("Synchronizing %d users" % len(ldap_users))
+        groups = self.get_groups()
+        users = self.get_users()
 
-        for ldap_user in ldap_users:
+        self.sync_groups(groups)
+        self.sync_users(users, groups)
+
+    def get_users(self):
+        """
+        Retrieve users from target LDAP server.
+        """
+        filterstr = "(&(objectCategory=person)(objectClass=user))"
+        attrlist = ['mailNickname', 'mail', 'givenName', 'sn', 'ipPhone', ]
+        page_size = 100
+        users = []
+
+        try:
+            l = ldap.initialize(settings.AUTH_LDAP_URI)
+            l.set_option(ldap.OPT_REFERRALS, 0)
+            l.protocol_version = ldap.VERSION3
+            l.simple_bind_s(settings.AUTH_LDAP_BASE_USER, settings.AUTH_LDAP_BASE_PASS)
+        except ldap.LDAPError, e:
+            LOG.error("Cannot connect to LDAP server: %s" % str(e))
+            return None
+
+        lc = SimplePagedResultsControl(ldap.LDAP_CONTROL_PAGE_OID, True, (page_size, ''))
+
+        while True:
+            msgid = l.search_ext(settings.AUTH_LDAP_BASE, ldap.SCOPE_SUBTREE, filterstr, attrlist, serverctrls=[lc])
+            rtype, rdata, rmsgid, serverctrls = l.result3(msgid)
+            for result in rdata:
+                users.append(result)
+            pctrls = [
+                c
+                for c in serverctrls
+                if c.controlType == ldap.LDAP_CONTROL_PAGE_OID
+            ]
+            if pctrls:
+                est, cookie = pctrls[0].controlValue
+                if cookie:
+                    lc.controlValue = (page_size, cookie)
+                else:
+                    break
+            else:
+                LOG.error("Server ignores RFC 2696 control")
+                break
+
+        l.unbind_s()
+
+        return users
+
+    def sync_users(self, users):
+        """
+        Synchronize users with local user database.
+        """
+        LOG.info("Synchronizing %d users" % len(users))
+
+        for ldap_user in users:
             try:
                 username = ldap_user[1]['mailNickname'][0]
             except:
@@ -47,72 +100,43 @@ class Command(NoArgsCommand):
                     user = User.objects.create_user(username, email)
                     user.first_name = first_name
                     user.last_name = last_name
-                    logger.info("User '%s' created" % username)
+                    LOG.info("User '%s' created" % username)
                 else:
                     if not user.first_name == first_name.decode('utf-8'):
                         user.first_name = first_name
-                        logger.info("User '%s' first name updated" % username)
+                        LOG.info("User '%s' first name updated" % username)
                     if not user.last_name == last_name.decode('utf-8'):
                         user.last_name = last_name
-                        logger.info("User '%s' last name updated" % username)
+                        LOG.info("User '%s' last name updated" % username)
                     if not user.email == email:
                         user.email = email
-                        logger.info("User '%s' email updated" % username)
+                        LOG.info("User '%s' email updated" % username)
                 user.save()
 
                 try:
                     profile = user.get_profile()
                 except (ObjectDoesNotExist, SiteProfileNotAvailable):
                     profile = UserProfile(user=user, id_num=id_num)
-                    logger.info("User '%s' profile created" % username)
+                    LOG.info("User '%s' profile created" % username)
                 else:
                     if not profile.id_num == id_num:
                         profile.id_num = id_num
-                        logger.info("User '%s' id number updated" % username)
+                        LOG.info("User '%s' id number updated" % username)
                 try:
                     profile.save()
                 except:
-                    logger.error("Duplicate ID '%s' encountered for '%s'" % (id_num, username))
+                    LOG.error("Duplicate ID '%s' encountered for '%s'" % (id_num, username))
 
-        logger.info("Users are synchronized")
+        LOG.info("Users are synchronized")
 
-    def get_ldap_users(self):
-        filterstr = "(&(objectCategory=person)(objectClass=user))"
-        attrlist = ['mailNickname', 'mail', 'givenName', 'sn', 'ipPhone', ]
-        page_size = 100
-        users = []
+    def get_groups(self):
+        """
+        Retrieve groups from target LDAP server.
+        """
+        pass
 
-        try:
-            l = ldap.initialize(settings.AUTH_LDAP_URI)
-            l.set_option(ldap.OPT_REFERRALS, 0)
-            l.protocol_version = ldap.VERSION3
-            l.simple_bind_s(settings.AUTH_LDAP_BASE_USER, settings.AUTH_LDAP_BASE_PASS)
-        except ldap.LDAPError, e:
-            logger.error("Cannot connect to LDAP server: %s" % str(e))
-            return None
-
-        lc = SimplePagedResultsControl(ldap.LDAP_CONTROL_PAGE_OID, True, (page_size, ''))
-
-        while True:
-            msgid = l.search_ext(settings.AUTH_LDAP_BASE, ldap.SCOPE_SUBTREE, filterstr, attrlist, serverctrls=[lc])
-            rtype, rdata, rmsgid, serverctrls = l.result3(msgid)
-            for result in rdata:
-                users.append(result)
-            pctrls = [
-                c
-                for c in serverctrls
-                if c.controlType == ldap.LDAP_CONTROL_PAGE_OID
-            ]
-            if pctrls:
-                est, cookie = pctrls[0].controlValue
-                if cookie:
-                    lc.controlValue = (page_size, cookie)
-                else:
-                    break
-            else:
-                logger.error("Server ignores RFC 2696 control")
-                break
-
-        l.unbind_s()
-
-        return users
+    def sync_groups(self):
+        """
+        Synchronize groups with local group database.
+        """
+        pass
