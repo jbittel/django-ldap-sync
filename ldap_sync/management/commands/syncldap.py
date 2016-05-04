7f530c48e10bac7b3d39a98c9d3d6913344cb030
Jason Bittel <jason.bittel@gmail.com>
2016-05-03 17:01:23 -0700

    Minor refactoring and reformatting

--- ldap_sync/management/commands/syncldap.py
+++ ldap_sync/management/commands/syncldap.py
@@ -9,14 +9,15 @@ from django.core.management.base import NoArgsCommand
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.core.exceptions import ImproperlyConfigured
-from django.db import IntegrityError, DataError
+from django.db import DataError
+from django.db import IntegrityError
 
 
 logger = logging.getLogger(__name__)
 
 
 class Command(NoArgsCommand):
-    help = "Synchronize users and groups from an authoritative LDAP server"
+    help = 'Synchronize users and groups from an authoritative LDAP server'
 
     def handle_noargs(self, **options):
         ldap_groups = self.get_ldap_groups()
@@ -28,69 +29,53 @@ class Command(NoArgsCommand):
             self.sync_ldap_users(ldap_users)
 
     def get_ldap_users(self):
-        """
-        Retrieve user data from target LDAP server.
-        """
+        """Retrieve user data from LDAP server."""
         user_filter = getattr(settings, 'LDAP_SYNC_USER_FILTER', None)
         if not user_filter:
-            msg = "LDAP_SYNC_USER_FILTER not configured, skipping user sync"
-            logger.info(msg)
+            logger.info('LDAP_SYNC_USER_FILTER not configured, skipping user sync')
             return None
 
-        attributes = getattr(settings, 'LDAP_SYNC_USER_ATTRIBUTES', None)
-        if not attributes:
-            error_msg = ("LDAP_SYNC_USER_ATTRIBUTES must be specified in "
-                         "your Django settings file")
-            raise ImproperlyConfigured(error_msg)
-        user_attributes = attributes.keys()
+        user_attributes = getattr(settings, 'LDAP_SYNC_USER_ATTRIBUTES', {})
+        if not user_attributes:
+            raise ImproperlyConfigured('LDAP_SYNC_USER_ATTRIBUTES must be specified in your Django settings')
 
-        users = self.ldap_search(user_filter, user_attributes)
+        users = self.ldap_search(user_filter, user_attributes.keys())
         logger.debug("Retrieved %d users" % len(users))
         return users
 
     def sync_ldap_users(self, ldap_users):
-        """
-        Synchronize users with local user database.
-        """
+        """Synchronize users with local user model."""
         model = get_user_model()
-        attributes = getattr(settings, 'LDAP_SYNC_USER_ATTRIBUTES', None)
+        user_attributes = getattr(settings, 'LDAP_SYNC_USER_ATTRIBUTES', None)
         username_field = getattr(settings, 'LDAP_SYNC_USERNAME_FIELD', None)
         if username_field is None:
             username_field = getattr(model, 'USERNAME_FIELD', 'username')
 
         if not model._meta.get_field(username_field).unique:
-            error_msg = ("Username field '%s' must be unique" % username_field)
+            raise ImproperlyConfigured("Field '%s' must be unique" % username_field)
+
+        if username_field not in user_attributes.values():
+            error_msg = ("LDAP_SYNC_USER_ATTRIBUTES must contain the field '%s'" % username_field)
             raise ImproperlyConfigured(error_msg)
 
-        if username_field not in attributes.values():
-            error_msg = ("LDAP_SYNC_USER_ATTRIBUTES must contain the "
-                         "username field '%s'" % username_field)
-            raise ImproperlyConfigured(error_msg)
-
-        for cname, attrs in ldap_users:
-            # In some cases with AD, attrs is a list instead of a
-            # dict; these are not valid users, so skip them
+        for cname, ldap_attributes in ldap_users:
+            model_attributes = {}
             try:
-                items = attrs.items()
+                for name, attribute in ldap_attributes.items():
+                    model_attributes[user_attributes[name]] = attribute[0].decode('utf-8')
             except AttributeError:
+                # In some cases attrs is a list instead of a dict; skip these invalid users
                 continue
 
-            # Extract user attributes from LDAP response
-            user_attr = {}
-            for name, attr in items:
-                user_attr[attributes[name]] = attr[0].decode('utf-8')
-
             try:
-                username = user_attr[username_field]
-                user_attr[username_field] = username.lower()
+                username = model_attributes[username_field]
             except KeyError:
-                logger.warning("User is missing a required attribute '%s'" %
-                               username_field)
+                logger.warning("User is missing a required attribute '%s'" % username_field)
                 continue
 
             kwargs = {
                 username_field + '__iexact': username,
-                'defaults': user_attr,
+                'defaults': model_attributes,
             }
 
             # Create or update user data in the local database
@@ -99,85 +84,70 @@ class Command(NoArgsCommand):
             except (IntegrityError, DataError) as e:
                 logger.error("Error creating user %s: %s" % (username, e))
             else:
-                updated_fields = []
+                update_fields = []
                 if created:
                     logger.debug("Created user %s" % username)
                     user.set_unusable_password()
-                    updated_fields.append('password')
+                    update_fields.append('password')
                 else:
-                    for name, attr in user_attr.items():
+                    for name, attr in model_attributes.items():
                         current_attr = getattr(user, name)
                         if current_attr != attr:
                             setattr(user, name, attr)
-                            updated_fields.append(name)
-                user.save(update_fields=updated_fields)
+                            update_fields.append(name)
+                user.save(update_fields=update_fields)
 
         logger.info("Users are synchronized")
 
     def get_ldap_groups(self):
-        """
-        Retrieve groups from target LDAP server.
-        """
+        """Retrieve groups from LDAP server."""
         group_filter = getattr(settings, 'LDAP_SYNC_GROUP_FILTER', None)
         if not group_filter:
-            msg = "LDAP_SYNC_GROUP_FILTER not configured, skipping group sync"
-            logger.info(msg)
+            logger.info('LDAP_SYNC_GROUP_FILTER not configured, skipping group sync')
             return None
 
-        attributes = getattr(settings, 'LDAP_SYNC_GROUP_ATTRIBUTES', None)
-        if not attributes:
-            error_msg = ("LDAP_SYNC_GROUP_ATTRIBUTES must be specified in "
-                         "your Django settings file")
-            raise ImproperlyConfigured(error_msg)
-        group_attributes = attributes.keys()
+        group_attributes = getattr(settings, 'LDAP_SYNC_GROUP_ATTRIBUTES', {})
+        if not group_attributes:
+            raise ImproperlyConfigured('LDAP_SYNC_GROUP_ATTRIBUTES must be specified in your Django settings')
 
-        groups = self.ldap_search(group_filter, group_attributes)
+        groups = self.ldap_search(group_filter, group_attributes.keys())
         logger.debug("Retrieved %d groups" % len(groups))
         return groups
 
     def sync_ldap_groups(self, ldap_groups):
-        """
-        Synchronize LDAP groups with local group database.
-        """
-        attributes = getattr(settings, 'LDAP_SYNC_GROUP_ATTRIBUTES', None)
+        """Synchronize LDAP groups with local group model."""
+        group_attributes = getattr(settings, 'LDAP_SYNC_GROUP_ATTRIBUTES', None)
         groupname_field = 'name'
 
-        if groupname_field not in attributes.values():
-            error_msg = ("LDAP_SYNC_GROUP_ATTRIBUTES must contain the "
-                         "group name field '%s'" % groupname_field)
+        if groupname_field not in group_attributes.values():
+            error_msg = "LDAP_SYNC_GROUP_ATTRIBUTES must contain the field '%s'" % groupname_field
             raise ImproperlyConfigured(error_msg)
 
-        for cname, attrs in ldap_groups:
-            # In some cases with AD, attrs is a list instead of a
-            # dict; these are not valid groups, so skip them
+        for cname, ldap_attributes in ldap_groups:
+            model_attributes = {}
             try:
-                items = attrs.items()
+                for name, attribute in ldap_attributes.items():
+                    model_attributes[group_attributes[name]] = attribute[0].decode('utf-8')
             except AttributeError:
+                # In some cases attrs is a list instead of a dict; skip these invalid groups
                 continue
-
-            # Extract user data from LDAP response
-            group_attr = {}
-            for name, attr in items:
-                group_attr[attributes[name]] = attr[0].decode('utf-8')
 
             try:
                 groupname = group_attr[groupname_field]
-                group_attr[groupname_field] = groupname.lower()
             except KeyError:
-                logger.warning("Group is missing a required attribute '%s'" %
-                               groupname_field)
+                logger.warning("Group is missing a required attribute '%s'" % groupname_field)
                 continue
 
             kwargs = {
                 groupname_field + '__iexact': groupname,
-                'defaults': group_attr,
+                'defaults': model_attributes,
             }
 
-            # Create or update group data in the local database
+            # Create group in the local database
             try:
                 group, created = Group.objects.get_or_create(**kwargs)
-            except IntegrityError as e:
-                logger.error("Error creating group %s" % e)
+            except (IntegrityError, DataError) as e:
+                logger.error("Error creating group %s: %s" % (groupname, e))
             else:
                 if created:
                     logger.debug("Created group %s" % groupname)
@@ -186,32 +156,27 @@ class Command(NoArgsCommand):
 
     def ldap_search(self, filter, attributes):
         """
-        Query the configured LDAP server with the provided search
-        filter and attribute list. Returns a list of the results
-        returned.
+        Query the configured LDAP server with the provided search filter and
+        attribute list.
         """
         uri = getattr(settings, 'LDAP_SYNC_URI', None)
         if not uri:
-            error_msg = ("LDAP_SYNC_URI must be specified in your Django "
-                         "settings file")
+            error_msg = 'LDAP_SYNC_URI must be specified in your Django settings file'
             raise ImproperlyConfigured(error_msg)
 
         base_user = getattr(settings, 'LDAP_SYNC_BASE_USER', None)
         if not base_user:
-            error_msg = ("LDAP_SYNC_BASE_USER must be specified in your "
-                         "Django settings file")
+            error_msg = 'LDAP_SYNC_BASE_USER must be specified in your Django settings file'
             raise ImproperlyConfigured(error_msg)
 
         base_pass = getattr(settings, 'LDAP_SYNC_BASE_PASS', None)
         if not base_pass:
-            error_msg = ("LDAP_SYNC_BASE_PASS must be specified in your "
-                         "Django settings file")
+            error_msg = 'LDAP_SYNC_BASE_PASS must be specified in your Django settings file'
             raise ImproperlyConfigured(error_msg)
 
         base = getattr(settings, 'LDAP_SYNC_BASE', None)
         if not base:
-            error_msg = ("LDAP_SYNC_BASE must be specified in your Django "
-                         "settings file")
+            error_msg = 'LDAP_SYNC_BASE must be specified in your Django settings file'
             raise ImproperlyConfigured(error_msg)
 
         ldap.set_option(ldap.OPT_REFERRALS, 0)
@@ -223,11 +188,7 @@ class Command(NoArgsCommand):
             logger.error("Error connecting to LDAP server %s" % uri)
             raise
 
-        results = l.paged_search_ext_s(base,
-                                       ldap.SCOPE_SUBTREE,
-                                       filter,
-                                       attrlist=attributes,
-                                       serverctrls=None)
+        results = l.paged_search_ext_s(base, ldap.SCOPE_SUBTREE, filter, attrlist=attributes, serverctrls=None)
         l.unbind_s()
         return results
 
@@ -239,19 +200,16 @@ class PagedResultsSearchObject:
     """
     page_size = getattr(settings, 'LDAP_SYNC_PAGE_SIZE', 100)
 
-    def paged_search_ext_s(self, base, scope, filterstr='(objectClass=*)',
-                           attrlist=None, attrsonly=0, serverctrls=None,
-                           clientctrls=None, timeout=-1, sizelimit=0):
+    def paged_search_ext_s(self, base, scope, filterstr='(objectClass=*)', attrlist=None, attrsonly=0,
+                           serverctrls=None, clientctrls=None, timeout=-1, sizelimit=0):
         """
         Behaves exactly like LDAPObject.search_ext_s() but internally uses the
         simple paged results control to retrieve search results in chunks.
         """
-        req_ctrl = SimplePagedResultsControl(True, size=self.page_size,
-                                             cookie='')
+        req_ctrl = SimplePagedResultsControl(True, size=self.page_size, cookie='')
 
         # Send first search request
-        msgid = self.search_ext(base, ldap.SCOPE_SUBTREE, filterstr,
-                                attrlist=attrlist,
+        msgid = self.search_ext(base, ldap.SCOPE_SUBTREE, filterstr, attrlist=attrlist,
                                 serverctrls=(serverctrls or []) + [req_ctrl])
         results = []
 
@@ -259,17 +217,14 @@ class PagedResultsSearchObject:
             rtype, rdata, rmsgid, rctrls = self.result3(msgid)
             results.extend(rdata)
             # Extract the simple paged results response control
-            pctrls = [c for c in rctrls if c.controlType ==
-                      SimplePagedResultsControl.controlType]
+            pctrls = [c for c in rctrls if c.controlType == SimplePagedResultsControl.controlType]
 
             if pctrls:
                 if pctrls[0].cookie:
                     # Copy cookie from response control to request control
                     req_ctrl.cookie = pctrls[0].cookie
-                    msgid = self.search_ext(base, ldap.SCOPE_SUBTREE,
-                                            filterstr, attrlist=attrlist,
-                                            serverctrls=(serverctrls or []) +
-                                            [req_ctrl])
+                    msgid = self.search_ext(base, ldap.SCOPE_SUBTREE, filterstr, attrlist=attrlist,
+                                            serverctrls=(serverctrls or []) + [req_ctrl])
                 else:
                     break
 
