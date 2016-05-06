8f078a9f5770c25e5bda933093d301b48c184a78
Jason Bittel <jason.bittel@gmail.com>
2016-05-06 09:34:40 -0700

    Simplify setup.py file

--- setup.py
+++ setup.py
@@ -1,39 +1,25 @@
 #!/usr/bin/env python
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import find_packages
+from setuptools import setup
 
-from ldap_sync import __version__ as version
-
-
-packages = [
-    'ldap_sync',
-    'ldap_sync.management',
-    'ldap_sync.management.commands',
-]
-
-package_data = {
-    '': ['LICENSE', 'README.rst'],
-}
 
 with open('README.rst') as f:
     readme = f.read()
 
 setup(
     name='django-ldap-sync',
-    version=version,
+    version='0.2.0',
     description='A Django application for synchronizing LDAP users and groups',
     long_description=readme,
+    license='BSD',
     author='Jason Bittel',
     author_email='jason.bittel@gmail.com',
     url='https://github.com/jbittel/django-ldap-sync',
-    download_url='https://github.com/jbittel/django-ldap-sync/downloads',
-    package_dir={'ldap-sync': 'ldap-sync'},
-    packages=packages,
-    package_data=package_data,
-    license='BSD',
+    download_url='https://github.com/jbittel/django-ldap-sync',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['python-ldap>=2.4.13'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
@@ -42,9 +28,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Topic :: System :: Systems Administration :: Authentication/Directory',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
     ],
     keywords=['django', 'ldap', 'active directory', 'synchronize', 'sync'],
-    install_requires=['python-ldap >= 2.4.13'],
 )
