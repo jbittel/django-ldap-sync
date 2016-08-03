dfc885784a869dc3f3ef200557be4303aa2752e9
Jason Bittel <jason.bittel@gmail.com>
2016-08-03 09:09:31 -0700

    Add callback to disable users by AD userAccountControl flags

--- ldap_sync/callbacks.py
+++ ldap_sync/callbacks.py
@@ -1,8 +1,30 @@
+def user_active_directory_deactivate(user, attributes, created, updated):
+    """
+    Deactivate user accounts based on Active Directory's
+    userAccountControl flags. Requires 'userAccountControl'
+    to be included in LDAP_SYNC_USER_EXTRA_ATTRIBUTES.
+    """
+    try:
+        user_account_control = int(attributes['userAccountControl'][0])
+        if user_account_control & 2:
+            user.is_active = False
+    except KeyError:
+        pass
+
+
 def removed_user_deactivate(user):
+    """
+    Deactivate user accounts that no longer appear in the
+    source LDAP server.
+    """
     if user.is_active:
         user.is_active = False
         user.save()
 
 
 def removed_user_delete(user):
+    """
+    Delete user accounts that no longer appear in the
+    source LDAP server.
+    """
     user.delete()
