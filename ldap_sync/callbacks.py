2290d4fff23ad465c993662fbc91d918725793a3
Jason Bittel <jason.bittel@gmail.com>
2016-10-04 14:40:12 -0700

    Also enable user accounts in AD callback

--- ldap_sync/callbacks.py
+++ ldap_sync/callbacks.py
@@ -1,6 +1,6 @@
-def user_active_directory_deactivate(user, attributes, created, updated):
+def user_active_directory_enabled(user, attributes, created, updated):
     """
-    Deactivate user accounts based on Active Directory's
+    Activate/deactivate user accounts based on Active Directory's
     userAccountControl flags. Requires 'userAccountControl'
     to be included in LDAP_SYNC_USER_EXTRA_ATTRIBUTES.
     """
@@ -8,6 +8,8 @@ def user_active_directory_deactivate(user, attributes, created, updated):
         user_account_control = int(attributes['userAccountControl'][0])
         if user_account_control & 2:
             user.is_active = False
+        else:
+            user.is_active = True
     except KeyError:
         pass
 
