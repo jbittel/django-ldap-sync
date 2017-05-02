4449d8c233c4a9ba74eb43adc1945ab45ae0a115
Jason Bittel <jason.bittel@gmail.com>
2017-05-02 10:23:52 -0700

    Use initialize() to instantiate LDAP connection

--- ldap_sync/search.py
+++ ldap_sync/search.py
@@ -1,7 +1,6 @@
 import logging
 
 import ldap
-from ldap.ldapobject import LDAPObject
 from ldap.controls import SimplePagedResultsControl
 
 
@@ -25,7 +24,7 @@ class LDAPSearch(object):
 
     def bind(self):
         ldap.set_option(ldap.OPT_REFERRALS, 0)
-        l = PagedLDAPObject(self.settings.URI)
+        l = ldap.initialize(self.settings.URI)
         l.protocol_version = ldap.VERSION3
         try:
             l.simple_bind_s(self.settings.BASE_USER, self.settings.BASE_PASS)
@@ -41,31 +40,28 @@ class LDAPSearch(object):
 
     def search(self, filterstr, attrlist):
         """Query the configured LDAP server."""
-        return self.ldap.paged_search_ext_s(self.settings.BASE, ldap.SCOPE_SUBTREE, filterstr=filterstr,
-                                            attrlist=attrlist, page_size=self.settings.PAGE_SIZE)
+        return self._paged_search_ext_s(self.settings.BASE, ldap.SCOPE_SUBTREE, filterstr=filterstr,
+                                        attrlist=attrlist, page_size=self.settings.PAGE_SIZE)
 
-
-class PagedResultsSearchObject:
-    """
-    Taken from the python-ldap paged_search_ext_s.py demo, showing how to use
-    the paged results control: https://bitbucket.org/jaraco/python-ldap/
-    """
-    def paged_search_ext_s(self, base, scope, filterstr='(objectClass=*)', attrlist=None, attrsonly=0,
-                           serverctrls=None, clientctrls=None, timeout=-1, sizelimit=0, page_size=10):
+    def _paged_search_ext_s(self, base, scope, filterstr='(objectClass=*)', attrlist=None, attrsonly=0,
+                            serverctrls=None, clientctrls=None, timeout=-1, sizelimit=0, page_size=10):
         """
-        Behaves exactly like LDAPObject.search_ext_s() but internally uses the
+        Behaves similarly to LDAPObject.search_ext_s() but internally uses the
         simple paged results control to retrieve search results in chunks.
+
+        Taken from the python-ldap paged_search_ext_s.py demo, showing how to use
+        the paged results control: https://bitbucket.org/jaraco/python-ldap/
         """
         req_ctrl = SimplePagedResultsControl(True, size=page_size, cookie='')
 
         # Send first search request
-        msgid = self.search_ext(base, scope, filterstr=filterstr, attrlist=attrlist, attrsonly=attrsonly,
-                                serverctrls=(serverctrls or []) + [req_ctrl], clientctrls=clientctrls,
-                                timeout=timeout, sizelimit=sizelimit)
+        msgid = self.ldap.search_ext(base, scope, filterstr=filterstr, attrlist=attrlist, attrsonly=attrsonly,
+                                     serverctrls=(serverctrls or []) + [req_ctrl], clientctrls=clientctrls,
+                                     timeout=timeout, sizelimit=sizelimit)
         results = []
 
         while True:
-            rtype, rdata, rmsgid, rctrls = self.result3(msgid)
+            rtype, rdata, rmsgid, rctrls = self.ldap.result3(msgid)
             results.extend(rdata)
             # Extract the simple paged results response control
             pctrls = [c for c in rctrls if c.controlType == SimplePagedResultsControl.controlType]
@@ -74,14 +70,10 @@ class PagedResultsSearchObject:
                 if pctrls[0].cookie:
                     # Copy cookie from response control to request control
                     req_ctrl.cookie = pctrls[0].cookie
-                    msgid = self.search_ext(base, scope, filterstr=filterstr, attrlist=attrlist, attrsonly=attrsonly,
-                                            serverctrls=(serverctrls or []) + [req_ctrl], clientctrls=clientctrls,
-                                            timeout=timeout, sizelimit=sizelimit)
+                    msgid = self.ldap.search_ext(base, scope, filterstr=filterstr, attrlist=attrlist,
+                                                 attrsonly=attrsonly, serverctrls=(serverctrls or []) + [req_ctrl],
+                                                 clientctrls=clientctrls, timeout=timeout, sizelimit=sizelimit)
                 else:
                     break
 
         return results
-
-
-class PagedLDAPObject(LDAPObject, PagedResultsSearchObject):
-    pass
